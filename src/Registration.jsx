80c64c3ad7bfc4d07160d1f5c6d6f3f8bcd37a15
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-05-02 13:07:12 -0400

    user can register

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -2,11 +2,100 @@ import { useState } from "react";
 import "./App.css";
 
 function Registration() {
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function goHome() {}
+  function goRegister() {}
+  function goLogIn() {}
+
+  async function submit(event) {
+    event.preventDefault();
+    console.log("First Name: ", firstname);
+    console.log(", Last Name: ", lastname);
+    console.log(", Email: ", email);
+    console.log(", Password: ", password);
+    const response = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({ firstname, lastname, email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+    if (response.ok) {
+      window.localStorage.setItem("token", json.token);
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setPassword("");
+    } else {
+      console.log(json);
+    }
+  }
+
   return (
     <>
-      <nav>- - - nav links - - -</nav>
+      <nav>
+        <ul>
+          <li>
+            <button onClick={() => goHome()}>Home</button>
+          </li>
+          <li>
+            <button onClick={() => goRegister()}>Register</button>
+          </li>
+          <li>
+            <button onClick={() => goLogIn()}>LogIn</button>
+          </li>
+        </ul>
+      </nav>
       <main>
-        <h1>Registration</h1>
+        <div className="page">
+          <h1>Registration</h1>
+          <form onSubmit={submit}>
+            <div className="form-group">
+              <label>First Name</label>
+              <input
+                value={firstname}
+                placeholder="First Name"
+                onChange={(ev) => setFirstName(ev.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Last Name</label>
+              <input
+                value={lastname}
+                placeholder="Last Name"
+                onChange={(ev) => setLastName(ev.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                value={email}
+                placeholder="email"
+                onChange={(ev) => setEmail(ev.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                value={password}
+                placeholder="password"
+                onChange={(ev) => setPassword(ev.target.value)}
+              />
+            </div>
+            <button
+              className="formBtn"
+              type="submit"
+              disabled={!email || !password}
+            >
+              Register
+            </button>
+          </form>
+        </div>
       </main>
     </>
   );
