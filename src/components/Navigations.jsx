2d9fba811e9cf3fa7c559a3a37c4aa38aefebfaa
Alyssa <ashinnickmitchell@gmail.com>
2025-05-05 21:09:03 -0600

    changed to navlinks

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../index.css";
+import "../app.css";
 
 export default function Navigations() {
   return (
-    <nav>
+    <nav className="navbar">
       <NavLink to="/">
         <h1>MyMoto</h1>
       </NavLink>
-      <div className="nav-links-container">
+      <div>
         <NavLink to="/register">Register</NavLink>
         <NavLink to="/login">Login</NavLink>
         <NavLink to="/user/:userId">My Account</NavLink>
