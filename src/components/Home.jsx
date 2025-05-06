9de379d5b86d9ac0eec32fb780de6c40bea5b4cf
Alyssa <ashinnickmitchell@gmail.com>
2025-05-05 21:47:53 -0600

    added ternary

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,25 @@
 import { GridBackground } from "../assets/grid";
 import { NavLink } from "react-router-dom";
+import { getToken } from "../app/tokenService";
+import { user } from "./SingleUser";
 import "../App.css";
 
 function Home() {
+  const token = getToken();
+
   return (
     <>
       <GridBackground />
       <div className="homePage">
-        <h1>
-          Please <NavLink to="/login">Sign-in</NavLink> or{" "}
-          <NavLink to="/register">Register</NavLink> to view users.
-        </h1>
+        {token === null ? (
+          <h1>
+            Please <NavLink to="/login">Sign-in</NavLink> or{" "}
+            <NavLink to="/register">Register</NavLink> to view users.
+          </h1>
+        ) : (
+          <></>
+          // if signed in, home page will return account info and list of users
+        )}
       </div>
     </>
   );
