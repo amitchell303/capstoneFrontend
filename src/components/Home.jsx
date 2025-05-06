ede2fa3773e4f9a8c94a660a34a42eb51c6905fe
Alyssa <ashinnickmitchell@gmail.com>
2025-05-06 09:01:43 -0600

    merged branches

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,8 @@
 import { GridBackground } from "../assets/grid";
-import { NavLink } from "react-router-dom";
-import "../App.css";
-
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useGetAllUsersQuery } from "./userSlice";
+import "../App.css";
 
 function Home() {
   const navigate = useNavigate();
@@ -30,31 +28,29 @@ function Home() {
     return <div>Error fetching users: {error.message}</div>;
   }
   return (
-    <>
-      <main id="homePage">
-        <GridBackground />
-        <div className="homePage">
-          <h1>
-            Please <NavLink to="/login">Sign-in</NavLink> or{" "}
-            <NavLink to="/register">Register</NavLink> to view users.
-          </h1>
-        </div>
-        <div className="page">
-          <h1>U S E R S</h1>
-          <ul>
-            {users.length > 0 ? (
-              users.map((user) => (
-                <li key={user.id}>
-                  {user.firstname} {user.lastname}{" "}
-                </li>
-              ))
-            ) : (
-              <li>No users found</li>
-            )}
-          </ul>
-        </div>
-      </main>
-    </>
+    <main id="homePage">
+      <GridBackground />
+      <div className="homePage">
+        <h1>
+          Please <NavLink to="/login">Sign-in</NavLink> or{" "}
+          <NavLink to="/register">Register</NavLink> to view users.
+        </h1>
+      </div>
+      <div className="page">
+        <h1>U S E R S</h1>
+        <ul>
+          {users.length > 0 ? (
+            users.map((user) => (
+              <li key={user.id}>
+                {user.firstname} {user.lastname}{" "}
+              </li>
+            ))
+          ) : (
+            <li>No users found</li>
+          )}
+        </ul>
+      </div>
+    </main>
   );
 }
 
