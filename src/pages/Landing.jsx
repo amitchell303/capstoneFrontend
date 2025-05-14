7287e2ed7c0badb83808c3de3b4f72fda2b2c98e
Alyssa <ashinnickmitchell@gmail.com>
2025-05-13 22:31:18 -0600

    DONE-landingPage & linked google fonts in html

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,57 +1,18 @@
 import { NavLink } from "react-router-dom";
-// import { useEffect } from "react";
-// import "../App.css";
-import "../landing.css";
+import "../styling/landing.css";
 
 export default function Landing() {
-  //   const navigate = useNavigate();
-  //   const token = localStorage.getItem("token");
-
-  //   useEffect(() => {
-  //     if (!token) {
-  //       navigate("/login");
-  //     } else {
-  //       if (data) {
-  //         setCars(data);
-  //         console.log(cars);
-  //       }
-  //     }
-  //   }, [token, navigate, data]);
-
-  //   if (!token) {
-  //     return (
-  //       <main id="homePage">
-  //         <div className="page">
-  //           <h1>
-  //             Please <NavLink to="/login">Sign-in</NavLink> or{" "}
-  //             <NavLink to="/register">Register</NavLink>.
-  //           </h1>
-  //         </div>
-  //       </main>
-  //     );
-  //   }
-
-  //   if (isLoading) {
-  //     return (
-  //       <main id="homePage">
-  //         <div className="page">
-  //           <div>Loading...</div>
-  //         </div>
-  //       </main>
-  //     );
-  //   }
-
   return (
     <div className="landingPage">
       <div className="text-content">
-        <h1 className="title">MyMoto</h1>
-        <h2>
+        <h1>MyMoto</h1>
+        <p>
           Please <NavLink to="/login">Sign-in</NavLink> or{" "}
           <NavLink to="/register">Register</NavLink>.
-        </h2>
+        </p>
       </div>
       <div className="animation">
-        <img src="src/components/assets/bmwbkgd.jpg" alt="BMW" />
+        <img src="src/components/assets/bmwbkgd.jpg" alt="BMW M3" />
       </div>
     </div>
   );
